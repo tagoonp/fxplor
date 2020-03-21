142de78957dd26115308678518323e6129dead9a
tagoon <tagoon.p@psu.ac.th>
2020-03-21 14:43:39 +0700

    20200321 14.43

--- common/fxplor/test5.php
+++ common/fxplor/test5.php
@@ -8,31 +8,31 @@
 
   </body>
   <script type="text/javascript">
-  const data = [
-    {
-      name: 'Bob',
-      gender: 'male',
-      age: 34,
-    },
-    {
-      name: 'Carol',
-      gender: 'female',
-      age: 36,
-    },
-    {
-      name: 'Ted',
-      gender: 'male',
-      age: 38,
-    },
-    {
-      name: 'Alice',
-      gender: 'female',
-      age: 40,
-    },
-    ];
-
-    const arr1 = data.filter(d => d.age < 34);
-    console.log('arr1', arr1);
+  // const data = [
+  //   {
+  //     name: 'Bob',
+  //     gender: 'male',
+  //     age: 34,
+  //   },
+  //   {
+  //     name: 'Carol',
+  //     gender: 'female',
+  //     age: 36,
+  //   },
+  //   {
+  //     name: 'Ted',
+  //     gender: 'male',
+  //     age: 38,
+  //   },
+  //   {
+  //     name: 'Alice',
+  //     gender: 'female',
+  //     age: 40,
+  //   },
+  //   ];
+  //
+  //   const arr1 = data.filter(d => d.age < 34);
+  //   console.log('arr1', arr1);
 
   </script>
 </html>
